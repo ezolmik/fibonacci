300eeac56e2343c7eaf134616b383c1cc054b809
ezolmik <ezolmik@gmail.com>
2016-08-07 22:18:13 +0200

    housekeeping

--- fibonacci-webapp/src/main/java/com/chemaxon/FibonacciWebApplication.java
+++ fibonacci-webapp/src/main/java/com/chemaxon/FibonacciWebApplication.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
